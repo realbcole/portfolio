664a2fd94e9b2c1374319ac68fb0186d8c12e4c2
realbcole <realbcole@gmail.com>
2023-05-08 20:09:13 -0400

    Bug fixes
    - Fixed Hire me button not being clickable on mobile

--- portfolio/src/components/HireMe.js
+++ portfolio/src/components/HireMe.js
@@ -7,7 +7,7 @@ const HireMe = () => {
     return (
         <div className='left-4 bottom-4 flex items-center justify-center
             overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto absolute
-            sm:right-0'>
+            sm:right-0 z-10'>
             <div className='w-48 h-auto flex items-center justify-center relative md:w-24'>
                 <CirclularText className={"fil-dark animate-spin-slow dark:fill-light"} />
                 <Link href="/contact" className="flex items-center justify-center
