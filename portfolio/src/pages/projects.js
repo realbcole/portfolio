f945807f9e76ce5184fa72c9684fbca0e1f79ed0
realbcole <realbcole@gmail.com>
2023-05-09 13:26:36 -0400

    Adjusted text size
    - Projects page project title on featured projects too small on mobile

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -31,7 +31,7 @@ const FeaturedProject = ({ type, title, techStack, summary, img, link, github })
                 <div className='w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6'>
                     <span className='text-primary font-medium text-xl dark:text-primaryDark xs:text-base'>{type}</span>
                     <Link href={link} className='hover:underline underline-offset-2'>
-                        <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm'>{title}</h2>
+                        <h2 className='my-2 w-full text-left text-4xl font-bold dark:text-light lg:text-3xl'>{title}</h2>
                     </Link>
                     <p className='font-semibold text-dark/90 dark:text-light/90 sm:text-sm'>
                         <span className='font-bold text-dark dark:text-light sm:text-sm'>Tech Stack: </span>
